690d7dfbc0003cd0a21cb0e52260a89649b90b8e
Mateus Furquim <furquim.axs@gmail.com>
2016-09-01 16:57:08 -0300

    Fixing user models' test. Changing and validating password
    Signed-off-by: Filipe Ribeiro <filiperibeirom7@gmail.com>
    Signed-off-by: Mateus Furquim <furquim.axs@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,44 +2,44 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
 
-	before do 
-		@user1 = User.new(id: 1, name: "user1")
+	before do
+		@user1 = User.new(id: 1, name: "user1", password: "abab")
 		@user1.save
-		@user2 = User.new(id: 2, name: "user2")
-		@user2.save
 	end
 
 	describe "#password" do
-		it "" do
-			expect(@password).to eq @new_password
+		it "changes the password" do
+			@user1.password = "cdcd"
+			expect(@user1.password).to eq "cdcd"
 		end
 	end
 
 	describe "#valid_password" do
-		it "" do
-			expect(@password_to_validate).to eq @password
+		it "validates the password" do
+			@user1.valid_password?("abab") == true
 		end
 
 	end
 
 
-	# before do 
+	# before do
 	# 	OmniAuth.config.test_mode = true
 	# 	omniauth_hash = { 'provider' => 'github',
-	# 	            'uid' => '12345',
+	# 	            'uid' => '2037375',
 	# 	            'info' => {
-	# 	                'name' => 'natasha',
-	# 	                'email' => 'hi@natashatherobot.com',
-	# 	                'image' => '~/Pictures/saymar.png'
+	# 	                'name' => 'Mateus Furquim',
+	# 	                'email' => 'furquim.axs@gmail.com',
+	# 	                'image' => 'https://avatars.githubusercontent.com/u/2037375?v=3'
 	# 	            }
 	# 	}
-
 	# 	OmniAuth.config.add_mock(:github, omniauth_hash)
+	# 	# auth = {'provider' => 'github',
+	# 	# 				'uid' => '2037375'}
 	# end
 
 	# describe "#find_or_create_with_omniauth" do
-	# 	it "" do
-	# 		expect(find_or_create_with_omniauth(@auth)).to eq @auth.uid
+	# 	it "github" do
+	# 		expect(find_or_create_with_omniauth(@auth)).to eq @user
 	# 		expect(auth.uid).to eq @user.auth.uid
 	# 	end
 	# end
