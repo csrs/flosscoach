1bd06c34ee0c7a8514c85c6db1b583aed371d9f9
João Guilherme <joaaogui@gmail.com>
2016-10-20 21:01:17 -0200

    Removing openhub_project model

--- app/models/openhub_project.rb
+++ app/models/openhub_project.rb
@@ -1,8 +1,8 @@
-class Project < ActiveResource::Base
-	    self.format =  ActiveResource::Formats::XmlFormat
-
-	self.site = "https://www.openhub.net/"
-
-
-
-end
+# class Project < ActiveResource::Base
+# 	    self.format =  ActiveResource::Formats::XmlFormat
+#
+# 	self.site = "https://www.openhub.net/"
+#
+#
+#
+# end
