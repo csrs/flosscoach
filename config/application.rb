d93ddc39abec9d527f5ffb73bdba1f3a3a87b429
João Guilherme <joaaogui@gmail.com>
2016-10-21 19:30:29 -0200

    Adding requirement (resolv-conf) and updating ci

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'active_resource'
+require "resolv-replace.rb"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -20,8 +21,8 @@ module Flosscoach
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.default_locale = "en" 
-    # config.i18n.default_locale = "pt-BR" 
+    config.i18n.default_locale = "en"
+    # config.i18n.default_locale = "pt-BR"
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
